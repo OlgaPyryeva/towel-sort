07e0a9c5a323021ec48bd4f445372d84fd678433
OlgaPyryeva <vashkevicholga@gmail.com>
2023-01-19 20:34:30 +0300

    feat: towelSort

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 // You should implement your task here.    module.exports = function towelSort(matrix)
-
 module.exports = function towelSort(matrix) {
     let str = "";
 
     if (arguments.length == 0 || matrix.length == 0) {
-        let matrix = [];
-        return matrix;
+        return [];
     } else {
         for (i = 0; i < matrix.length; ) {
             str += matrix[i].join();
+
             if (i < matrix.length - 1) {
                 str += "," + matrix[i + 1].reverse().join() + ",";
             }
             i += 2;
         }
-        let number = +str.match(/\d/g).join("");
-        matrix = ("" + number).split("").map(Number);
+        matrix = str.split(",").map(Number);
+        matrix = matrix.filter(Boolean);
     }
-
     return matrix;
 };
